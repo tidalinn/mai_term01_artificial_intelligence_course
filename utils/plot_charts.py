d86f02fc253abeba9dbc2efb51c52836cfe2d122
Gradt <linn.doodles@gmail.com>
2022-11-17 01:25:45 +0300

    Finished all tasks

--- utils/plot_charts.py
+++ utils/plot_charts.py
@@ -7,6 +7,7 @@ from sklearn.metrics import mean_squared_error
 
 import matplotlib.pyplot as plt
 from matplotlib import cm
+import matplotlib.patheffects as pe
 
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
@@ -29,11 +30,11 @@ def plot_scatter(x, y, title: str):
     plt.show()
 
 
-def plot_gradient_descent_loss_2d(x, y,
-                                  weights: list,
-                                  title: str,
-                                  m_range = np.arange(-30, 60, 2),
-                                  b_range = np.arange(-40, 120, 2)):
+def plot_contour_interactive_dataset_2d(x, y,
+                                        weights: list,
+                                        title: str,
+                                        m_range = np.arange(-30, 60, 2),
+                                        b_range = np.arange(-40, 120, 2)):
     
     # dimension
     if x.ndim == 1:
@@ -116,11 +117,11 @@ def plot_gradient_descent_loss_2d(x, y,
     return fig
 
 
-def plot_gradient_descent_function_2d(x, y,
-                                      weights: list,
-                                      loss_f: Mapping,
-                                      title: str,
-                                      global_min: list = None):
+def plot_contour_interactive_custom_func_2d(x, y,
+                                            weights: list,
+                                            loss_f: Mapping,
+                                            title: str,
+                                            global_min: list = None):
     
     x_meshed, y_meshed = np.meshgrid(x, y)
     weights = np.array(weights)
@@ -232,13 +233,104 @@ def plot_function_2d(x, y,
         ax.grid()
 
 
-def plot_gradient_descent_3d(x, y,
-                             loss_f: Mapping,
-                             title: str,
-                             weights = None,
-                             descent: bool = False,
-                             ax = None,
-                             global_min: list = None):
+def plot_contour_2d(x, y,
+                    loss_f: Mapping,
+                    weights: list,
+                    title: str,
+                    global_min: list = None):
+    
+    font_s = 16
+    
+    x_meshed, y_meshed = np.meshgrid(x, y)
+    z = loss_f(x_meshed, y_meshed)
+    
+    
+    # plot color
+    levels = np.linspace(np.min(z), np.max(z), 20)
+    
+    
+    # descent
+    weights = np.array(weights)
+    intercepts, slopes = weights[:, 0], weights[:, 1]
+
+    start_point = intercepts[0], slopes[0]
+    end_point = intercepts[-1], slopes[-1]
+    
+
+    # background
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.subplots()
+    
+    
+    # function body
+    ax.contourf(x_meshed, 
+                y_meshed, 
+                z, 
+                levels=levels, 
+                cmap=cm.coolwarm,
+                alpha=0.9)
+
+
+    # descent
+    ax.plot(intercepts, 
+            slopes,
+            color='orange',
+            path_effects=[pe.Stroke(linewidth=5, foreground='black'), pe.Normal()],
+            linewidth=3,
+            label='Descent')
+
+    
+    # start point
+    ax.scatter(*start_point,
+               color='red', 
+               path_effects=[pe.Stroke(linewidth=5, foreground='black'), pe.Normal()],
+               s=500,
+               label='Start',
+               zorder=10)
+
+    
+    # end point
+    ax.scatter(*end_point,
+               color='yellow',
+               marker='*',
+               path_effects=[pe.Stroke(linewidth=5, foreground='black'), pe.Normal()],
+               s=500,
+               label='End',
+               zorder=10)
+    
+    
+    # global minimum
+    if global_min is not None:
+        ax.scatter(global_min[0],
+                   global_min[1],
+                   color='white',
+                   path_effects=[pe.Stroke(linewidth=5, foreground='black'), pe.Normal()],
+                   s=200,
+                   label='Global Min')
+
+    
+    ax.legend(loc='lower right',
+              labelspacing=1.6,
+              borderpad=1,
+              fontsize='large')
+        
+        
+    # axes
+    ax.set_title(f'{title}\n', fontsize=font_s + 2)
+
+    ax.set_xlabel('w0', fontsize=font_s)
+    ax.set_ylabel('w1', fontsize=font_s)
+
+    plt.show()
+
+
+def plot_custom_function_3d(x, y,
+                            loss_f: Mapping,
+                            title: str,
+                            weights = None,
+                            descent: bool = False,
+                            ax = None,
+                            global_min: list = None):
     
     font_s = 16    
     x_meshed, y_meshed = np.meshgrid(x, y)
@@ -255,7 +347,7 @@ def plot_gradient_descent_3d(x, y,
                     y_meshed, 
                     loss_f(x_meshed, y_meshed), 
                     alpha=0.6, 
-                    cmap=cm.coolwarm)
+                    cmap=cm.coolwarm,)
     
 
     if descent == True:
@@ -271,23 +363,36 @@ def plot_gradient_descent_3d(x, y,
         ax.plot(intercepts, 
                 slopes, 
                 loss_f(intercepts, slopes),
-                color='blue',
+                color='orange',
+                path_effects=[pe.Stroke(linewidth=4, foreground='black'), pe.Normal()],
+                linewidth=2,
                 label='Descent')
         
+
         # start point
         ax.scatter(*start_point,
                    loss_f(*start_point), 
                    color='red', 
-                   s=200,
+                   path_effects=[pe.Stroke(linewidth=4, foreground='black'), pe.Normal()],
+                   s=300,
                    label='Start')
         
+
         # end point
         ax.scatter(*end_point,
                    loss_f(*end_point),
-                   color='coral',
+                   color='yellow',
                    marker='*',
-                   s=200,
+                   path_effects=[pe.Stroke(linewidth=4, foreground='black'), pe.Normal()],
+                   s=300,
                    label='End')
+        
+        
+        # legend
+        ax.legend(loc='upper right',
+                  labelspacing=1.6,
+                  borderpad=1,
+                  fontsize='large')
         
 
     # global minimum
@@ -295,10 +400,8 @@ def plot_gradient_descent_3d(x, y,
         ax.scatter(global_min[0],
                    global_min[1],
                    color='black',
-                   s=200,
+                   s=300,
                    label='Global Min')
-        
-        ax.legend(loc='upper right')
     
 
     # axes
@@ -334,8 +437,8 @@ def plot_function_gradient_2d_3d(x, y,
     
     # second subplot
     ax2 = fig.add_subplot(122, projection='3d')
-    plot_gradient_descent_3d(x, y, loss_f, 
-                             f'3D {title} function',
-                             weights,
-                             descent,
-                             ax=ax2)
+    plot_custom_function_3d(x, y, loss_f, 
+                            f'3D {title} function',
+                            weights,
+                            descent,
+                            ax=ax2)
